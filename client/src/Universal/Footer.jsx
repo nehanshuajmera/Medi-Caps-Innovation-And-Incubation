5eff22b6c53450830b46cf8c24987e8a287e4244
Shyam Patidar <qshyam>
2023-03-28 02:12:50 +0530

    offering page

--- client/src/Universal/Footer.jsx
+++ client/src/Universal/Footer.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import './Footer.css'
-export default function Footer() {
-  return (
-    <div>
-      <footer id="Footer" className="clearfix" role="contentinfo">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <hr className="no_line" style={{margin: '0 auto 120px'}} /> </div>
-            <div className="col-md-4">
-              <h6>Contact us</h6>
-              <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
-              <h4 className="has-text-color" style={{color: '#017fe4'}}><a href="tel:61383766284">+61 (0) 383 766 284</a></h4>
-              <p> <a href="#"><u><span>noreply@envato.com</span></u></a> </p>
-            </div>
-            <div className="col-md-4 mobile-one tablet-one-third">
-              <h6>Links</h6>
-              <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
-              <p style={{marginBottom: '30px'}}> <a href="about.html" data-type="URL" data-id="about/index.html">Our company</a> </p>
-              <p style={{marginBottom: '30px'}}> <a href="news.html" data-type="URL" data-id="news/index.html">News</a> </p>
-              <p style={{marginBottom: '30px'}}> <a href="contact.html" data-type="URL" data-id="contact/index.html">Contact</a> </p>
-              <p style={{marginBottom: '30px'}}> <a href="#">Terms of use</a> </p>
-            </div>
-            <div className="col-md-4 mobile-one tablet-one-third">
-              <figure className="wp-block-image size-full"><img src="images/lottie.svg" className="wp-image-26" /> </figure>
-              <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
-              <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam. </p>
-              <div style={{height: '25px'}} aria-hidden="true" className="wp-block-spacer" />
-              <p> <u><a href="#">Read more</a></u> </p>
-            </div>
-          </div>
-        </div>
-        <div className="footer_copy">
-          <div className="container">
-            <div className="column one mobile-one">
-              <div className> <a id="back_to_top" className="footer_button" href="#"><i className="icon-up-open-big" aria-label="Back to top icon" /></a>
-                <div className="copyright"> © 2022 - BeTheme. Muffin group - HTML by <a target="_blank" rel="nofollow" href="https://1.envato.market/9ZxXY">BeantownThemes</a> </div>
-                <ul className="social" role="navigation" aria-label="Social Menu" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
-  )
-}
+// import React from 'react'
+// import './Footer.css'
+// export default function Footer() {
+//   return (
+//     <div>
+//       <footer id="Footer" className="clearfix" role="contentinfo">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-12">
+//               <hr className="no_line" style={{margin: '0 auto 120px'}} /> </div>
+//             <div className="col-md-4">
+//               <h6>Contact us</h6>
+//               <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
+//               <h4 className="has-text-color" style={{color: '#017fe4'}}><a href="tel:61383766284">+61 (0) 383 766 284</a></h4>
+//               <p> <a href="#"><u><span>noreply@envato.com</span></u></a> </p>
+//             </div>
+//             <div className="col-md-4 mobile-one tablet-one-third">
+//               <h6>Links</h6>
+//               <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
+//               <p style={{marginBottom: '30px'}}> <a href="about.html" data-type="URL" data-id="about/index.html">Our company</a> </p>
+//               <p style={{marginBottom: '30px'}}> <a href="news.html" data-type="URL" data-id="news/index.html">News</a> </p>
+//               <p style={{marginBottom: '30px'}}> <a href="contact.html" data-type="URL" data-id="contact/index.html">Contact</a> </p>
+//               <p style={{marginBottom: '30px'}}> <a href="#">Terms of use</a> </p>
+//             </div>
+//             <div className="col-md-4 mobile-one tablet-one-third">
+//               <figure className="wp-block-image size-full"><img src="images/lottie.svg" className="wp-image-26" /> </figure>
+//               <div style={{height: '45px'}} aria-hidden="true" className="wp-block-spacer" />
+//               <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam. </p>
+//               <div style={{height: '25px'}} aria-hidden="true" className="wp-block-spacer" />
+//               <p> <u><a href="#">Read more</a></u> </p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="footer_copy">
+//           <div className="container">
+//             <div className="column one mobile-one">
+//               <div className> <a id="back_to_top" className="footer_button" href="#"><i className="icon-up-open-big" aria-label="Back to top icon" /></a>
+//                 <div className="copyright"> © 2022 - BeTheme. Muffin group - HTML by <a target="_blank" rel="nofollow" href="https://1.envato.market/9ZxXY">BeantownThemes</a> </div>
+//                 <ul className="social" role="navigation" aria-label="Social Menu" />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </footer>
+//     </div>
+//   )
+// }
