97d43a69f85984f342d83cf91b0a26837aaf229e
sharma39vishal <sharma39vishal@gmail.com>
2023-04-07 17:18:39 +0530

    scroll top

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import Header from './Universal/Header';
 import Footer from './Universal/Footer';
 import Home from './Pages/Herosection';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Contact from './Pages/Contact';
 import Article from './Pages/Article';
 import Event from './Pages/Event';
 import Offering from './Pages/Offering';
 import About from './Pages/About';
 import Singleeventpage from './SingleEvent/Singleeventpage';
+import { useEffect } from 'react';
 
 function App() {
+  
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [pathname]);
+
   return (
     <div>
       <Header/>
