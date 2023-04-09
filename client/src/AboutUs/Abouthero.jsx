5774e6a44e89542c2740d4194b23d262335027d0
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-04-09 17:10:16 +0530

    About, Herosection, Event, And About-Hero

--- client/src/AboutUs/Abouthero.jsx
+++ client/src/AboutUs/Abouthero.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import '../Pages/About.css'
 export default function Abouthero() {
   return (
 
     <div className="about-section-1 about-hero-img">
       <div className="about-image-container">
-        <img src="./images/about-1-mu.jpeg" alt="" />
+        <img src="./images/about-1-mu(copy).jpeg" alt="" center cover />
       </div>
-      
-      <h6 className='section-1-heading'>About Us</h6>
-      <p className="section-1-paragraph">"Create a perfect ecosystem for your business"</p>
+      <h6 className='about-section-1-heading'>About Us</h6>
+      <p className="about-section-1-paragraph">"Create a perfect ecosystem for your business"</p>
     </div>
   )
 }
--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -8,17 +8,13 @@ export default function About() {
       <Abouthero/>
  
       <div className="about-section-2">
-        <img src="https://media.licdn.com/dms/image/C560BAQFVLpC7QtyrAg/company-logo_400_400/0/1676459347608?e=2147483647&v=beta&t=8NlPHtSImrzq0W1AEugSxgD3YlwLjuB6FGVexwzSk-o" alt="" width={"70%"} />
+        <img src="./images/Black logo - no background.png" alt="" width={"35%"} />
         <div className="divide-and-conquer-2">
         <p className="about-section-2-paragraph">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut error numquam quasi repellendus in delectus quibusdam nulla, assumenda ipsum quisquam quo, quae odio quis! Sed perferendis eligendi dolore quos, ea est quia ducimus culpa?
         </p>
         </div>
       </div>
-      {/* <div className="about-section-3">
-        <h2 className="ourteam">Our Team</h2>
-        <SingleTeamMember/>
-      </div> */}
       <TeamMember/>
     </div>
   )
