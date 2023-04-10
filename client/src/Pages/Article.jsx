73630c3ab702f9967161ad3f8fff4e3db1e1e0fd
Shyam Patidar <qshyam>
2023-04-10 23:11:03 +0530

    responsive about

--- client/src/Pages/Article.jsx
+++ client/src/Pages/Article.jsx
@@ -4,8 +4,8 @@ export default function Article() {
   return (
     <div>
       <div className="article-container">
-        <div className="blog-img">
-          <img
+        <div className="blog-img ">
+          <img className="removeonphone"
             src="https://www.startupindia.gov.in/content/dam/invest-india/PGF/IncubatorPage_banner_v2.gif"
             alt="blog-img"
             />
@@ -49,7 +49,7 @@ export default function Article() {
             </p>
           </div>
         </div>
-        <div className="blog-continer-3">
+        <div className="blog-continer-1">
           <div className="blog-section-1-img">
             <img
               src="https://media.istockphoto.com/id/1187369229/vector/staff-or-people-education-business-incubator-business-training-programs-start-up-development.jpg?s=612x612&w=0&k=20&c=3zT7Vv0SJvcpTceeRilE7_WaCk70IFAykZ40BkoUH2I="
@@ -68,7 +68,7 @@ export default function Article() {
             </p>
           </div>
         </div>
-        <div className="blog-continer-4">
+        <div className="blog-continer-2">
           <div className="blog-section-1-img">
             <img
               src="https://media.istockphoto.com/id/1187369229/vector/staff-or-people-education-business-incubator-business-training-programs-start-up-development.jpg?s=612x612&w=0&k=20&c=3zT7Vv0SJvcpTceeRilE7_WaCk70IFAykZ40BkoUH2I="
@@ -87,7 +87,7 @@ export default function Article() {
             </p>
           </div>
         </div>
-        <div className="blog-continer-5">
+        <div className="blog-continer-1">
           <div className="blog-section-1-img">
             <img
               src="https://media.istockphoto.com/id/1187369229/vector/staff-or-people-education-business-incubator-business-training-programs-start-up-development.jpg?s=612x612&w=0&k=20&c=3zT7Vv0SJvcpTceeRilE7_WaCk70IFAykZ40BkoUH2I="
