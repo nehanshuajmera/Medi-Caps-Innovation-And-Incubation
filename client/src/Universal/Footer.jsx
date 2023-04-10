ff3fede310da7f7cccbfa16ce10fbe6c96b05740
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-04-10 16:49:16 +0530

    build

--- client/src/Universal/Footer.jsx
+++ client/src/Universal/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './Footer.css'
 export default function Footer() {
   return (
     <div>
 
       
-      {/* <div className="pg-footer">
+      <div className="pg-footer">
         <footer className="footer">
           <svg className="footer-wave-svg" viewBox="0 0 1200 100" preserveAspectRatio="none">
             <path className="footer-wave-path" d="M851.8,100c125,0,288.3-45,348.2-64V0H0v44c3.7-1,7.3-1.9,11-2.9C80.7,22,151.7,10.8,223.5,6.3C276.7,2.9,330,4,383,9.8 c52.2,5.7,103.3,16.2,153.4,32.8C623.9,71.3,726.8,100,851.8,100z" />
@@ -133,7 +133,7 @@ export default function Footer() {
             </div>
           </div>
         </footer>
-      </div> */}
+      </div>
     </div>
   )
 }
