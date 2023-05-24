8ae005a06b22ec33eeaeb9d98c3bcd0da5b1a172
unknown <parmarsinghbhanu@gmail.com>
2023-05-24 21:56:51 +0530

    frontend client pages pastevent me change

--- client/src/Pages/Event.jsx
+++ client/src/Pages/Event.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Event.css';
 import { useNavigate } from 'react-router-dom';
 import Pasteventbox from './Pasteventbox';
 import AOS from 'aos';
+import axios from "axios";
 import 'aos/dist/aos.css';
 AOS.init();
 
+
 export default function Event() {
+
+// const Event = () => {
+//   const[event,setevent]=useState();
+ 
+// const getevent=async ()=>{
+//     await axios.get(".../backend/router/event").then((res) =>
+//      { setevents(res.data) });
+//   }
+//   useEffect(() => {
+//     getevent();
+//   }, [])
+
+
+
   const navigate = useNavigate();
   return (
     <div>
@@ -32,11 +48,16 @@ export default function Event() {
          <Pasteventbox/>
          <Pasteventbox/>
          <Pasteventbox/>
+         <Pasteventbox/>
+         
       </div>
       <div className='past-events scroll-sections'>
          <Pasteventbox/>
          <Pasteventbox/>
          <Pasteventbox/>
+         {/* {events.map((item) => {
+         return <events id={item._id} title={item.title} description={item.description} images={item.images} authername={item.authername} date={item.date} time={item.time} location={item.location} numberofspeaker={item.numberofspeaker} registrationfee={item.registrationfee}/>
+        })} */}
       </div>
       </div>
     </div>
