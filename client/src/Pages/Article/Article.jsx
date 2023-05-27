e10adf412d3a319c560432353a5ee8adc4430ed7
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-05-27 19:54:25 +0530

    Restructuring

--- client/src/Pages/Article/Article.jsx
+++ client/src/Pages/Article/Article.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useEffect, useState } from 'react'
 import "./Article.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import axios from "axios";
-AOS.init();
 
 export default function Article() {
 
