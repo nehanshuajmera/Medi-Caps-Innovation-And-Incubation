22b2ada7fe20fd478c53b9e644a32d5659d32145
sharma39vishal <sharma39vishal@gmail.com>
2023-05-28 14:18:42 +0530

    rebuild

--- client/src/Admin/Pages/ContactQuery/SingleContact.jsx
+++ client/src/Admin/Pages/ContactQuery/SingleContact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SingleContact({props}) {
   return (
     <div className='single-query-box'>
-        <h2>Name: {props.name}  Phone: {props.phone}</h2>
+        <h2>Name: {props.name} ,  Phone: {props.phone}</h2>
         <h2>Email: {props.email}</h2>
         <h2>Query: {props.query}</h2>
         <h2>Idea: {props.idea}</h2>
