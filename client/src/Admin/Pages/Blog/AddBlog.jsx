23c411edc308d5116ede8de2abf5c28a3b70dc95
sharma39vishal <sharma39vishal@gmail.com>
2023-05-28 20:09:53 +0530

    image upload

--- client/src/Admin/Pages/Blog/AddBlog.jsx
+++ client/src/Admin/Pages/Blog/AddBlog.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
+import { v4 } from 'uuid';
+import { storage } from '../../../firebase';
+import {getDownloadURL, listAll, ref, uploadBytes} from 'firebase/storage';
 
 const AddBlog = () => {
   
@@ -12,7 +15,29 @@ const AddBlog = () => {
     date:"",
   });
   
+  const [images,setimages]=useState([]);
+  const [imageUpload,setImageUpload]=useState(null);
   
+  const checkimg=()=>{
+    // console.log(imageUpload);
+    if(imageUpload.size>200000){
+      return false;
+    }
+    const fileExtension = imageUpload.name.split(".").at(-1);
+    const allowedFileTypes = ["jpg", "png","gif","jpeg"];
+    if (!allowedFileTypes.includes(fileExtension)) {
+        return false;
+    }  
+    return true;
+}
+
+  const afterurl=async(url)=>{
+    setFormData({
+      ...FormData,
+      images: url
+  })
+    alert("Image was Succesfully Updated");
+  }  
  
   const handleChange = e => {
       const { name, value } = e.target
@@ -21,6 +46,7 @@ const AddBlog = () => {
           [name]: value
       })
   }
+
   async function submit() {
     try {
       await axios.post("/blog/addblog",FormData)
@@ -31,6 +57,36 @@ const AddBlog = () => {
     }
   }
   
+  
+  const changeimage=async ()=>{
+    // console.log("change image");
+    try{
+    if(!checkimg()){
+        alert("Please Upload Valid image on 200KB");
+    }
+    else if(imageUpload!==null){
+        const imageRef=ref(storage,'files/'+v4()+imageUpload.name);
+        await uploadBytes(imageRef,imageUpload).then((snapshot)=>{
+          getDownloadURL(snapshot.ref).then((url)=>{
+            // console.log(url); 
+             afterurl(url);
+          })
+        }) 
+    }
+    else{
+        console.log("nothing")
+    }
+   //  console.log(user);
+      } catch (err) {
+     console.error(err);
+       }
+    }
+
+    useEffect(() => {
+      // console.log("img change")
+        changeimage();
+    }, [imageUpload])
+
 
   return (
     <div>
@@ -54,7 +110,7 @@ const AddBlog = () => {
                   <label htmlFor="event-img">
                     <i class="fa-solid fa-upload"/>
                   </label>
-                  <input type="file" name="event-img" id="" />
+                  <input type="file" name="event-img" accept="image/png, image/gif, image/jpeg"  onChange={(event)=>{setImageUpload(event.target.files[0])}} />
                 </div>
                 <button onClick={()=>{submit()}} id='blog-txt-add' >Add</button>
                 <button type="reset">Clear</button>
--- client/src/Admin/Pages/Event/AddEvent.jsx
+++ client/src/Admin/Pages/Event/AddEvent.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
+import { v4 } from 'uuid';
+import { storage } from '../../../firebase';
+import {getDownloadURL, listAll, ref, uploadBytes} from 'firebase/storage';
+
+
 const AddEvent = () => {
 
 
@@ -15,9 +20,30 @@ const AddEvent = () => {
     numberofspeaker:"",
     registrationfee:"",
   });
+  const [images,setimages]=useState([]);
+  const [imageUpload,setImageUpload]=useState(null);
   
-  
- 
+  const checkimg=()=>{
+    // console.log(imageUpload);
+    if(imageUpload.size>200000){
+      return false;
+    }
+    const fileExtension = imageUpload.name.split(".").at(-1);
+    const allowedFileTypes = ["jpg", "png","gif","jpeg"];
+    if (!allowedFileTypes.includes(fileExtension)) {
+        return false;
+    }  
+    return true;
+}
+
+  const afterurl=async(url)=>{
+    setFormData({
+      ...FormData,
+      images: url
+  })
+    alert("Image was Succesfully Updated");
+  }
+
   const handleChange = e => {
       const { name, value } = e.target
       setFormData({
@@ -36,6 +62,34 @@ const AddEvent = () => {
   }
   
 
+  const changeimage=async ()=>{
+    // console.log("change image");
+    try{
+    if(!checkimg()){
+        alert("Please Upload Valid image on 200KB");
+    }
+    else if(imageUpload!==null){
+        const imageRef=ref(storage,'files/'+v4()+imageUpload.name);
+        await uploadBytes(imageRef,imageUpload).then((snapshot)=>{
+          getDownloadURL(snapshot.ref).then((url)=>{
+            // console.log(url); 
+             afterurl(url);
+          })
+        }) 
+    }
+    else{
+        console.log("nothing")
+    }
+   //  console.log(user);
+      } catch (err) {
+     console.error(err);
+       }
+    }
+
+    useEffect(() => {
+      // console.log("img change")
+        changeimage();
+    }, [imageUpload])
 
 
   return (
@@ -65,7 +119,7 @@ const AddEvent = () => {
                   <label htmlFor="event-img">
                     <i class="fa-solid fa-upload"/>
                   </label>
-                  <input type="file" name="event-img" id="" />
+                  <input type="file" name="event-img"  accept="image/png, image/gif, image/jpeg"  onChange={(event)=>{setImageUpload(event.target.files[0])}} />
                 </div>
                 <button onClick={()=>{submit()}} id='blog-txt-add' >Add</button>
                 <button type="reset">Clear</button>
