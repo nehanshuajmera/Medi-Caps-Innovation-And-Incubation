f7f4a9472d0dcbdd4b0c48d07056e134c0640875
sharma39vishal <sharma39vishal@gmail.com>
2023-05-29 19:41:58 +0530

    new mongodb uri

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
 const path=require("path");
 app.use(express.static('client/build'));
-
-app.get('*', (req,res) => {
-  res.sendFile(path.resolve( 'client', 'build','index.html'));
-})
+ app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client','build','index.html'));
+});
