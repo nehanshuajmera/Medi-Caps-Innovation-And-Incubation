ad0a0343b94f64eafa9c01692334b1e252e56f80
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-06-15 18:37:39 +0530

    Pull

--- client/src/Pages/Home/Carosol/Ourpartner.jsx
+++ client/src/Pages/Home/Carosol/Ourpartner.jsx
@@ -9,12 +9,10 @@ function Ourpartner() {
         <div className="home-sec6-main">
           <div className="home-sec6-start">
             <h1 className="home-sec6-h1" style={{ fontSize: '50px', fontFamily: 'Inter' }}>From concept to wide Adoption</h1>
-            {/* <h1 className="home-sec6-h1">to wide adoption</h1> */}
             <Images />
             <p className="home-sec6-text">MII Foundation is a full-scale incubator that prepares decentralized innovation for global use. To prepare the project for wide adoption, we assure full compliance, set up collaborations with global financial institutions, investment funds, governments. MII is a space where highly practical ideas that could potentially impact millions
               (or better, billions) receive the support they deserve.
             </p>
-            {/* <button className="home-sec6-button">Read more</button> */}
           </div>
         </div>
       </div>
