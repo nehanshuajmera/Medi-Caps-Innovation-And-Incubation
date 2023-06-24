1e4444e8456054c13953482bdfee49b9b9800e1e
Aman <amansahu5051@gmail.com>
2023-06-24 22:11:17 +0530

    scroll animation

--- client/src/Pages/Home/Scroll Animation/ScrollAnimation.jsx
+++ client/src/Pages/Home/Scroll Animation/ScrollAnimation.jsx
@@ -3,9 +3,10 @@ import "./scrollanimation.css"
 import { useInView } from 'react-intersection-observer';
 
 export default function ScrollAnimation() {
+ 
   // const [i1,isvisible ] = useInView();
   // const{ref : i2, inView : isvisible2}=useInView();
-
+  
   const myref1 = useRef();
   const myref2 = useRef();
   const myref3 = useRef();
@@ -15,6 +16,10 @@ export default function ScrollAnimation() {
   const[isvisible3,setISvisible3] = useState(false);
   const[isvisible4,setISvisible4] = useState(false);
 
+  const apperOptions = {
+    // threshold : 1,
+    rootMargin : "0px 0px -100px 0px"
+  };
   useEffect(()=>{
     // console.log('myref',myref.current);
     const observer1 = new IntersectionObserver((entries)=>{
@@ -25,7 +30,8 @@ export default function ScrollAnimation() {
         console.log("entry",entry);
       });
       
-    })
+    },apperOptions)
+
     const observer2 = new IntersectionObserver((entries)=>{
       // const entry =entries[0];
       // console.log('entry', entry);
@@ -34,7 +40,8 @@ export default function ScrollAnimation() {
         console.log("entry",entry);
       });
       
-    })
+    },apperOptions)
+
     const observer3 = new IntersectionObserver((entries)=>{
       // const entry =entries[0];
       // console.log('entry', entry);
@@ -43,7 +50,8 @@ export default function ScrollAnimation() {
         console.log("entry",entry);
       });
       
-    })
+    },apperOptions)
+
     const observer4 = new IntersectionObserver((entries)=>{
       // const entry =entries[0];
       // console.log('entry', entry);
@@ -51,38 +59,50 @@ export default function ScrollAnimation() {
         setISvisible4(entry.isIntersecting);
         console.log("entry",entry);
       })
-    })
+    },apperOptions)
+
     observer1.observe(myref1.current);
     observer2.observe(myref2.current);
     observer3.observe(myref3.current);
     observer4.observe(myref4.current);
+   
 })
+ // X
+ const [x, setX] = useState();
+
+ // Y
+ const [y, setY] = useState();
+
+ // This function calculate X and Y
+ const getPosition = (Ref) => {
+   const x = Ref.current.offsetLeft;
+   setX(x);
+
+   const y = Ref.current.offsetTop;
+   setY(y);
+ };
+   useEffect(()=>{
+     window.addEventListener("scroll",getPosition(myref2));
+     // console.log(myref2.current.getBoundingClientRect().top); 
+     // var position = document.documentElement
+     // console.log("from top: " , myref2.current.offsetTop);
+   })
+
   return (
     <div className='scroll-animation-homepage'>
       <div className="scroll-animation-homepage-left">
         <div className="scroll-animation-homepage-container"> 
           <div ref={myref1} className="scroll-animation-homepage-container-content">
             <h1>Ideate </h1>
-            {/* <i className={`fa-solid fa-1 fa-9x  ${isvisibleOdd?'show-animation':'hide-animation'}`}/> */}
-            {/* <i className={`fa-solid fa-1 fa-9x hidden-i-animation ${isvisible1?'show-i-animation ':''}`}/> */}
-            {/* <i className={`fa-solid fa-1 fa-9x `}/> */}
           </div>
           <div ref={myref2} className="scroll-animation-homepage-container-content">
             <h1>Innovate </h1>
-            {/* <i className={`fa-solid fa-2 fa-9x  ${isvisibleEven?'show-animation':'hide-animation'}`}/> */}
-            {/* <i className={`fa-solid fa-2 fa-9x hidden-i-animation ${isvisible2?'show-i-animation ':''}`}/> */}
           </div>
           <div ref={myref3} className="scroll-animation-homepage-container-content">
             <h1>Incubate </h1>
-            {/* <i className={`fa-solid fa-3 fa-9x  ${isvisibleOdd?'show-animation':'hide-animation'}`}/> */}
-            {/* <i className={`fa-solid fa-3 fa-9x hidden-i-animation ${isvisible3?'show-i-animation ':''}`}/> */}
-            {/* <i classname="fa-solid fa-3 fa-9x hidden-i-animation"></i> */}
           </div>
           <div ref={myref4} className="scroll-animation-homepage-container-content">
             <h1>Accelerate </h1>
-            {/* <i className={`fa-solid fa-4 fa-9x  ${isvisibleEven?'show-animation':'hide-animation'}`}/> */}
-            {/* <i className={`fa-solid fa-4 fa-9x hidden-i-animation ${isvisible4?'show-i-animation ':''}`}/> */}
-            {/* <i classname="fa-solid fa-4 fa-9x hidden-i-animation"></i> */}
           </div>
         </div>
       </div>     
@@ -93,25 +113,5 @@ export default function ScrollAnimation() {
         <i className={`fa-solid fa-4 fa-9x hidden-i-animation ${isvisible4?'show-i-animation ':''}`}/>
       </div>
     </div>
-    // <div className='scroll-animation-homepage'>
-    //   <div className="scroll-animation-homepage-container"> 
-    //     <div ref={i1} className="scroll-animation-homepage-container-content">
-    //       <h1>Ideate ${isvisible?'Yes':''}</h1>
-    //       <i className={`fa-solid fa-1 fa-9x hidden-i-animation ${isvisible?'show-i-animation':''}`}/>
-    //     </div>
-    //     <div ref={i2} className="scroll-animation-homepage-container-content">
-    //       <h1>Innovate</h1>
-    //       <i className={`fa-solid fa-2 fa-9x hidden-i-animation ${isvisible2?'show-i-animation':''}`}/>
-    //     </div>
-    //     <div className="scroll-animation-homepage-container-content">
-    //       <h1>Incubate</h1>
-    //       <i className="fa-solid fa-3 fa-9x hidden-i-animation"></i>
-    //     </div>
-    //     <div className="scroll-animation-homepage-container-content">
-    //       <h1>Accelerate</h1>
-    //       <i className="fa-solid fa-4 fa-9x hidden-i-animation"></i>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
