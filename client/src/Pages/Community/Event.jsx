630ff67c167f4c5e4a381f50bf2e44dad1b40c60
unknown <parmarsinghbhanu@gmail.com>
2023-07-04 17:01:29 +0530

    community page added

--- client/src/Pages/Community/Event.jsx
+++ client/src/Pages/Community/Event.jsx
@@ -1,19 +1,71 @@
 import React from 'react'
-import './Event.css'
+import './Event.css';
 
 const event = () => {
   return (
     <div>
-    <h2><span className="eventlist-title-date">Tomorrow</span></h2>
-    <div class="event-card-body">
-    <h3 class="event-card-summary">Independence Day holiday</h3>
-<p class="event-card-time" data-event-start="2023-07-04T00:00:00" data-event-allday="true"><span class="svg-container"><svg viewBox="0 0 20 20" class="svg-clock">
-      <title>Day</title>
-	  </svg></span>Tue 7/4</p>
-<div class="event-card-flex">
-</div>
-<p class="event-card-category">UCLA<span class="subcalendar"><span class="subcalendar-spacer"> • </span><a href="/ucla/holidays" title="Holidays">Holidays</a></span></p>
-</div>  
+      <div className="event-container">
+      
+        <div className="todays-event">
+          <h2 className="event-today-header">Today`s Event </h2>
+               <div className="today-event-box">
+               <div className="event-box-container">
+                <h3 className='event-box-header'>
+                Independence Day holiday
+                </h3>
+                <p className="event-box-para">
+                <i className="fa-sharp fa-solid fa-location-dot"></i> 
+               Rau Indore
+                  <li>Holiday</li>
+                </p>
+               </div>
+               </div>
+               </div>
+       
+        <div className="tommarow-event">
+          <h2 className="event-tommarow-header">tomarow event </h2>
+               <div className="today-event-box">
+               <div className="event-box-container">
+                <h3 className='event-box-header'>
+                Independence Day holiday
+                </h3>
+                <p className="event-box-para">
+                {/* <i className="fa-sharp fa-solid fa-location-dot"></i>  */}
+               {/* <p className="rau-class"> Rau Indore</p> */}
+              rau indore
+                 
+                </p>
+                 <li>Holiday</li>
+               </div>
+               <div className="event-box-container">
+                <h3 className='event-box-header'>
+               {/* <i className="fa-regular fa-location-dot" style={{fontweight: 'bold'}}></i> */}
+                {/* <FontAwesomeIcon icon="fa-sharp fa-regular fa-location-dot" /> */}
+                Independence Day holiday
+                </h3>
+                <p className="event-box-para">
+               
+                <i className="fa-sharp fa-solid fa-location-dot"></i> 
+                Rau Indore
+                  <li>Holiday</li>
+                </p>
+                
+               </div>
+               <div className="event-box-container">
+                <h3 className='event-box-header'>
+                Independence Day holiday
+                </h3>
+                <p className="event-box-para">
+                <i className="fa-sharp fa-solid fa-location-dot">  </i> 
+                Rau Indore
+                  <li>Holiday</li>
+                </p>
+                
+              
+               </div>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
